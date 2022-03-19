bad5b649453a1a134f483af0e781222f52242ada
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-20 01:31:13 +0900

    chore(*): final set

--- src/wdyr.js
+++ src/wdyr.js
@@ -1,10 +1,14 @@
-import * as React from 'react'
+import React from 'react'
 
 if (process.env.NODE_ENV === 'development') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render')
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    logOwnerReasons: true,
-    logOnDifferentValues: true,
-  })
+  try {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+      logOwnerReasons: true,
+      logOnDifferentValues: true,
+    })
+  } catch (error) {
+    console.debug(error)
+  }
 }
