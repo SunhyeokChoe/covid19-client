88c28c6f04bc901016a4c61a3c3eb4aa70d10d12
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-20 01:42:43 +0900

    feat(*): final set

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
-      // tsx: true,
     },
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -32,32 +31,22 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    // '@typescript-eslint/explicit-member-accessibility': 'off',
-    // '@typescript-eslint/no-object-literal-type-assertion': 'off',
-
-    // v4 changes
-    // 'no-use-before-define': 'off',
-    // '@typescript-eslint/no-use-before-define': ['error'],
-    // 'no-shadow': 'off',
-    // '@typescript-eslint/no-shadow': ['error'],
-
     // React
     'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
     'react/prop-types': ['off', {}],
     // import
-    // 'import/extensions': [
-    //   'error',
-    //   'ignorePackages',
-    //   {
-    //     js: 'never',
-    //     mjs: 'never',
-    //     jsx: 'never',
-    //     ts: 'never',
-    //     tsx: 'never',
-    //   },
-    // ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     'import/prefer-default-export': 'off',
-    'import/extensions': ['off'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
@@ -68,12 +57,13 @@ module.exports = {
        * You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
        * It will default to "latest" and warn if missing, and to "detect" in the future
        */
-      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      version: 'detect',
     },
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       },
     },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
   },
 }
